890cca8d41cede17e181c5ac6a816217a7785807
Diego Maroto <diego.diemg@gmail.com>
2018-11-21 21:51:07 +0100

    Refactor para no tener que hacer npm i por cada ejercicio. La fiesta de la escritura en disco.

--- typescript/space-age/space-age.ts
+++ typescript/space-age/space-age.ts
@@ -1,35 +1,13 @@
 export default class SpaceAge {
-    [k: string]: any;
-    private static readonly EARTH_SECONDS_IN_YEAR = 31557600;
-    private static readonly PLANET_YEAR_SCALES = new Map<string, number>([
-      ['Earth', 1],
-      ['Mercury', 0.2408467],
-      ['Venus', 0.61519726],
-      ['Mars', 1.8808158],
-      ['Jupiter', 11.862615],
-      ['Saturn', 29.447498],
-      ['Uranus', 84.016846],
-      ['Neptune', 164.79132]
-    ]);
-  
-    public seconds: number;
-  
-    constructor(ageInSeconds: number) {
-      this.seconds = ageInSeconds;
-  
-      for (const [stellarBody, scale] of SpaceAge.PLANET_YEAR_SCALES) {
-        const funcName = 'on' + stellarBody;
-        SpaceAge.prototype[funcName] = () => {
-          return this.scaleToPlanetYears(scale);
-        };
-      }
-    }
-  
-    private scaleToPlanetYears(scaleRelativeToEarthYears: number): number {
-      return +(
-        this.seconds /
-        SpaceAge.EARTH_SECONDS_IN_YEAR /
-        scaleRelativeToEarthYears
-      ).toFixed(2);
-    }
-  }
+    private earth_day_seconds: number = 31557600
+    constructor(public seconds: number) { }
+    onEarth = (): number => this.getRelation(1)
+    onMercury = (): number => this.getRelation(0.2408467)
+    onVenus = (): number => this.getRelation(0.61519726)
+    onMars = (): number => this.getRelation(1.8808158)
+    onJupiter = (): number => this.getRelation(11.862615)
+    onSaturn = (): number => this.getRelation(29.447498)
+    onUranus = (): number => this.getRelation(84.016846)
+    onNeptune = (): number => this.getRelation(164.79132)
+    getRelation = (relation: number): number => Math.round(this.seconds / this.earth_day_seconds / relation * 100) / 100
+}
